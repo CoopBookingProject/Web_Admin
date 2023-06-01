ad0a4a1819f15546e37965a5393f29c558099721
b6134143 <brakasut@gmail.com>
2023-06-01 16:21:35 +0700

    ui forgotpassword - close #7

--- fontend/src/routes/MainRoutes.jsx
+++ fontend/src/routes/MainRoutes.jsx
@@ -3,17 +3,20 @@ import { lazy } from "react";
 import MainLayout from "../layout/MainLayout";
 
 const LoginPage = lazy(() => import("../pages/auth/Login"));
-
+const ForgotPasswordPage = lazy(() => import("../pages/auth/ForgotPassword")); // Import the ForgotPassword page component
 
 const AdminRoutes = () => {
     return {
         path: "/",
         element: <MainLayout />,
         children: [
-  
             {
                 path: "/",
                 element: <LoginPage />,
+            },
+            {
+                path: "/ForgotPassword", // Add the path for the ForgotPassword page
+                element: <ForgotPasswordPage />, // Add the ForgotPassword page component
             },
         ],
     };
