efcd8b822911f9ec54a0d58080262f1e4fdc9407
unknown <b6301774@g.sut.ac.th>
2023-06-13 15:04:32 +0700

    ui cut - close #14

--- fontend/src/pages/admin/Massage/MassageTable/Allmassage.jsx
+++ fontend/src/pages/admin/Massage/MassageTable/Allmassage.jsx
@@ -1,4 +1,6 @@
-import { MenuOutlined } from '@ant-design/icons';
+import React, { useState } from 'react';
+import { Button, Badge, Input, Modal, Form, Row, Col, Radio } from 'antd';
+import { MenuOutlined, FormOutlined, DeleteOutlined } from '@ant-design/icons';
 import { DndContext } from '@dnd-kit/core';
 import {
   arrayMove,
@@ -7,128 +9,144 @@ import {
   verticalListSortingStrategy,
 } from '@dnd-kit/sortable';
 import { CSS } from '@dnd-kit/utilities';
-import { Badge } from 'antd';
-import React, { useState } from 'react';
-import { TableSize, StyledTable } from "./TableStyled";
-import { FormOutlined, DeleteOutlined } from "@ant-design/icons";
-import { Select, Button } from "antd";
+import { TableSize, StyledTable } from './TableStyled';
 
-const columns = [
-  {
-    title: 'ID',
-    dataIndex: 'id',
-    width: 50,
-    align: "center",
-  },
-  {
-    title: 'ชื่อ',
-    dataIndex: 'name',
-    width: 200,
-  },
-  {
-    title: 'นามสกุล',
-    dataIndex: 'surname',
-    width: 200,
-  },
-  {
-    title: 'ทักษะการนวด',
-    dataIndex: 'skill',
-    width: 125,
-    align: "center",
-  },
-  {
-    title: 'สถานะ',
-    dataIndex: 'status',
-    key: 'status',
-    width: 125,
-    
-    render: status => {
-      let color;
-      let text;
-  
-      switch (status) {
-        case 'Idle':
-          color = 'success';
-          text = 'ว่าง';
-          break;
-        case 'Process':
-          color = 'error';
-          text = 'กำลังใหบริการ';
-          break;
-        default:
-          color = 'default';
-          text = 'ออฟไลน์';
+const { TextArea } = Input;
+
+const EditModal = ({ visible, record, onCancel, onOk }) => {
+  const [form] = Form.useForm();
+
+  const handleOk = () => {
+    form.validateFields().then((values) => {
+      form.resetFields();
+      onOk({ ...record, ...values });
+    });
+    onCancel();
+  };
+
+  return (
+    <Modal
+      visible={visible}
+      onCancel={onCancel}
+      onOk={handleOk}
+      destroyOnClose
+      centered
+      maskStyle={{ backgroundColor: 'rgba(0, 0, 0, 0.5)' }}
+      bodyStyle={{ backgroundColor: '#fff', padding: '20px 20px 5px', borderRadius: '5px' }}
+      width={650}
+      title={
+        <h1 style={{ textAlign: 'center', marginBottom: '10px' }}>
+          แก้ไขข้อมูลผู้นวด
+        </h1>
       }
-  
-      return <Badge status={color} text={text} />;
-    },
-  },
-  {
-    title: "เเก้ไขข้อมูล",
-    dataIndex: "edit",
-    key: "edit",
-    width: 100,
-    align: "center",
-    render: (text, record) => (
-      <Button
-        onClick={() => handleEdit(record)}
-        icon={<FormOutlined />}
-        style={{
-          border: "none",
-          background: "transparent",
-          color: "#000",
-          boxShadow: "none",
-        }}
-      />
-    ),
-  },
-  {
-    title: "ลบ",
-    dataIndex: "delete",
-    key: "delete",
-    width: 100,
-    align: "center",
-    render: (text, record) => (
-      <Button
-        onClick={() => handleDelete(record)}
-        icon={<DeleteOutlined />}
-        style={{
-          border: "none",
-          background: "transparent",
-          color: "#000",
-          boxShadow: "none",
-        }}
-      />
-    ),
-  },
-  {
-    title: "Action",
-    key: 'sort',
-    width: 50,
-    align: "center",
-  },
-];
+      footer={[
+        <div key="footer-buttons" style={{ textAlign: 'center' }}>
+          <Button
+    key="submit"
+    type="primary"
+    onClick={handleOk}
+    style={{
+    borderRadius: '48px',
+    backgroundColor: '#617A55',
+    color: '#fff',
+    height: '50px',
+    width: '125px',
+    transition: 'background-color 0.3s ease',
+  }}
+  onMouseEnter={(e) => {
+    e.target.style.backgroundColor = '#496841';
+  }}
+  onMouseLeave={(e) => {
+    e.target.style.backgroundColor = '#617A55';
+  }}
+>
+  บันทึก
+</Button>
+        </div>
+      ]}
+    >
+      {/* Modal content */}
+      <Form form={form} layout="vertical">
+        <Row gutter={[16, 16]}>
+          <Col span={12}>
+            <Form.Item label="ชื่อ" name="name">
+              <Input
+                style={{
+                  height: '40px',
+                  borderRadius: '48px',
+                  backgroundColor: '#C2D9BD',
+                }}
+                placeholder=""
+              />
+            </Form.Item>
+          </Col>
+          <Col span={12}>
+            <Form.Item label="นามสกุล" name="surname">
+              <Input
+                style={{
+                  height: '40px',
+                  borderRadius: '48px',
+                  backgroundColor: '#C2D9BD',
+                }}
+                placeholder=""
+              />
+            </Form.Item>
+          </Col>
+          <Col span={12}>
+            <Form.Item label="อายุ" name="age">
+              <Input
+                style={{
+                  height: '40px',
+                  borderRadius: '48px',
+                  backgroundColor: '#C2D9BD',
+                }}
+                placeholder=""
+              />
+            </Form.Item>
+          </Col>
+          <Col span={12}>
+          <Form.Item label="เพศ" name="gender">
+          <Radio.Group>
+            <Radio value="male">ชาย</Radio>
+            <Radio value="female">หญิง</Radio>
+            <Radio value="other">เพศทางเลือก</Radio>
+          </Radio.Group>
+        </Form.Item>
+        </Col>
+      
+          {/* Add more form fields here */}
+        </Row>
+        <Col>
+        <Form.Item label="ประสบการณ์ในการทำงาน" name="additionalInfo">
+          <TextArea
+            style={{
+              borderRadius: '8px',
+              backgroundColor: '#C2D9BD',
+              
+            }}
+            placeholder="กรุณากรอกข้อมูล"
+            rows={6}
+          />
+        </Form.Item>
+        </Col>
+      </Form>
+    </Modal>
+  );
+};
 
-const Row = ({ children, ...props }) => {
+const SortableRow = ({ children, ...props }) => {
   const {
     attributes,
     listeners,
     setNodeRef,
-    setActivatorNodeRef,
     transform,
     transition,
     isDragging,
-  } = useSortable({
-    id: props['data-row-key'],
-  });
+  } = useSortable({ id: props['data-row-key'] });
+
   const style = {
     ...props.style,
-    transform: CSS.Transform.toString(
-      transform && {
-        ...transform,
-        scaleY: 1,
-      },
-    )?.replace(/translate3d\(([^,]+),/, 'translate3d(0,'),
+    transform: CSS.Transform.toString(transform),
     transition,
     ...(isDragging
       ? {
@@ -137,6 +155,7 @@ const Row = ({ children, ...props }) => {
         }
       : {}),
   };
+
   return (
     <tr {...props} ref={setNodeRef} style={style} {...attributes}>
       {React.Children.map(children, (child) => {
@@ -144,7 +163,6 @@ const Row = ({ children, ...props }) => {
           return React.cloneElement(child, {
             children: (
               <MenuOutlined
-                ref={setActivatorNodeRef}
                 style={{
                   touchAction: 'none',
                   cursor: 'move',
@@ -160,17 +178,10 @@ const Row = ({ children, ...props }) => {
   );
 };
 
-const handleEdit = (record) => {
+const AllMassage = () => {
+  const [showModal, setShowModal] = useState(false);
+  const [selectedRecord, setSelectedRecord] = useState(null);
 
-  console.log("Edit clicked for record:", record);
-};
-
-const handleDelete = (record) => {
-
-  console.log("Delete clicked for record:", record);
-};
-
-const Allmassage = () => {
   const [dataSource, setDataSource] = useState([
     {
       key: '1',
@@ -189,6 +200,14 @@ const Allmassage = () => {
       status: 'Idle',
     },
   ]);
+
+  const handleEditSave = (editedRecord) => {
+    setDataSource((previousDataSource) =>
+      previousDataSource.map((record) => (record.key === editedRecord.key ? editedRecord : record))
+    );
+    handleCloseModal();
+  };
+
   const onDragEnd = ({ active, over }) => {
     if (active.id !== over?.id) {
       setDataSource((previous) => {
@@ -198,30 +217,145 @@ const Allmassage = () => {
       });
     }
   };
-  return (
-    
-    <DndContext onDragEnd={onDragEnd}>
-      <SortableContext
-        // rowKey array
-        items={dataSource.map((i) => i.key)}
-        strategy={verticalListSortingStrategy}
-      >
-       <TableSize> 
-        <StyledTable
-          components={{
-            body: {
-              row: Row,
-            },
-          }}
-          rowKey="key"
-          columns={columns}
-          dataSource={dataSource}
+
+  const handleEditClick = (record) => {
+    setSelectedRecord(record);
+    setShowModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+  };
+
+  const handleDelete = (record) => {
+    console.log('Delete clicked for record:', record);
+  };
+
+  const columns = [
+    {
+      title: 'ID',
+      dataIndex: 'id',
+      width: 50,
+      align: 'center',
+    },
+    {
+      title: 'ชื่อ',
+      dataIndex: 'name',
+      width: 200,
+    },
+    {
+      title: 'นามสกุล',
+      dataIndex: 'surname',
+      width: 200,
+    },
+    {
+      title: 'ทักษะการนวด',
+      dataIndex: 'skill',
+      width: 125,
+      align: 'center',
+    },
+    {
+      title: 'สถานะ',
+      dataIndex: 'status',
+      key: 'status',
+      width: 125,
+      render: (status) => {
+        let color;
+        let text;
+
+        switch (status) {
+          case 'Idle':
+            color = 'success';
+            text = 'ว่าง';
+            break;
+          case 'Process':
+            color = 'error';
+            text = 'กำลังให้บริการ';
+            break;
+          default:
+            color = 'default';
+            text = 'ออฟไลน์';
+        }
+
+        return <Badge status={color} text={text} />;
+      },
+    },
+    {
+      title: 'แก้ไขข้อมูล',
+      dataIndex: 'edit',
+      key: 'edit',
+      width: 100,
+      align: 'center',
+      render: (_, record) => (
+        <Button
+          onClick={() => handleEditClick(record)}
+          icon={<FormOutlined />}
+          type="text"
+          style={{ color: '#000' }}
         />
-        </TableSize>
-      </SortableContext>
-    </DndContext>
+      ),
+    },
+    {
+      title: 'ลบ',
+      dataIndex: 'delete',
+      key: 'delete',
+      width: 100,
+      align: 'center',
+      render: (_, record) => (
+        <Button
+          onClick={() => handleDelete(record)}
+          icon={<DeleteOutlined />}
+          type="text"
+          style={{ color: '#000' }}
+        />
+      ),
+    },
+    {
+      title: 'Action',
+      key: 'sort',
+      width: 50,
+      align: 'center',
+      render: (_, record) => (
+        <MenuOutlined
+          style={{
+            touchAction: 'none',
+            cursor: 'move',
+          }}
+        />
+      ),
+    },
+  ];
+
+  return (
+    <TableSize>
+    <DndContext onDragEnd={onDragEnd}>
+      <SortableContext items={dataSource} strategy={verticalListSortingStrategy}>
+        
+      <StyledTable
+  components={{
+    body: {
+      row: SortableRow,
+    },
+  }}
+  dataSource={dataSource}
+  columns={columns}
+  pagination={{
     
+    
+    
+  }}
+/>
+      </SortableContext>
+
+      <EditModal
+  visible={showModal}
+  record={selectedRecord}
+  onCancel={handleCloseModal}
+  onOk={handleEditSave}
+/>
+    </DndContext>
+  </TableSize>
   );
 };
 
-export default Allmassage;
+export default AllMassage;
--- fontend/src/pages/admin/Massage/index.jsx
+++ fontend/src/pages/admin/Massage/index.jsx
@@ -1,64 +1,203 @@
 import React, { useState } from "react";
+import { Button, Input, Modal, Form, Row, Col, Radio } from 'antd';
 import {
-    MassageHeader,
-    Iconbutton,
-    Bar1,
-    Bar2,
-    MassageBody,
-    TableFillter,
-    Line1,
-    Line2,
-    MainTable,
+  MassageHeader,
+  Iconbutton,
+  Bar1,
+  Bar2,
+  MassageBody,
+  TableFillter,
+  Line1,
+  Line2,
+  MainTable,
 } from "./MassageStyled";
 import Allmassage from "./MassageTable/Allmassage";
 import AllCancelled from "./MassageTable/AllCancelled";
 
+const { TextArea } = Input;
+const { useForm } = Form;
+
 const Massage = () => {
-    const [activeMenu, setActiveMenu] = useState("ผู้นวดทั้งหมด");
+  const [activeMenu, setActiveMenu] = useState("ผู้นวดทั้งหมด");
+  const [modalVisible, setModalVisible] = useState(false);
 
-    const handleTableFillterClick = (menu) => {
-        setActiveMenu(menu);
-    };
+  const [form] = useForm(); // Declare the form object
 
-    return (
-        <>
-            <MassageHeader>
-                <h1>ผู้นวด</h1>
-                <Iconbutton>
-                    <Bar1></Bar1>
-                    <Bar2></Bar2>
-                    <p style={{ marginLeft: '63px' }}>เพิ่มผู้นวด</p>
-                </Iconbutton>
-            </MassageHeader>
-            <MassageBody>
-                <TableFillter
-                    onClick={() => handleTableFillterClick("ผู้นวดทั้งหมด")}
-                    isActive={activeMenu === "ผู้นวดทั้งหมด"}
-                >
-                    ผู้นวดทั้งหมด
-                </TableFillter>
-                <TableFillter
-                    onClick={() => handleTableFillterClick("Cancelled")}
-                    isActive={activeMenu === "Cancelled"}
-                >
-                    Cancelled
-                </TableFillter>
-                <Line1></Line1>
-                {activeMenu && (
-                    <Line2
-                        style={{ marginLeft: activeMenu === "ผู้นวดทั้งหมด" ? "0px" : "110px" }}
-                    ></Line2>
-                )}
-                <MainTable>
-                    {activeMenu === "ผู้นวดทั้งหมด" && <Allmassage />}
-                    {activeMenu === "Cancelled" && <AllCancelled />}
-                </MainTable>       
-            </MassageBody>
-            
-        </>
-    );
+  const handleTableFillterClick = (menu) => {
+    setActiveMenu(menu);
+  };
+
+  const handleAddMassageClick = () => {
+    setModalVisible(true);
+  };
+
+  const handleModalOk = () => {
+    // Handle OK button click
+    setModalVisible(false);
+  };
+
+  const handleModalCancel = () => {
+    // Handle Cancel button click
+    setModalVisible(false);
+  };
+
+  return (
+    <>
+      <MassageHeader>
+        <h1>ผู้นวด</h1>
+        <Iconbutton onClick={handleAddMassageClick}>
+          <Bar1 />
+          <Bar2 />
+          <p style={{ marginLeft: "63px" }}>เพิ่มผู้นวด</p>
+        </Iconbutton>
+      </MassageHeader>
+      <MassageBody>
+        <TableFillter
+          onClick={() => handleTableFillterClick("ผู้นวดทั้งหมด")}
+          isActive={activeMenu === "ผู้นวดทั้งหมด"}
+        >
+          ผู้นวดทั้งหมด
+        </TableFillter>
+        <TableFillter
+          onClick={() => handleTableFillterClick("Cancelled")}
+          isActive={activeMenu === "Cancelled"}
+        >
+          Cancelled
+        </TableFillter>
+        <Line1 />
+        {activeMenu && (
+          <Line2
+            style={{
+              marginLeft: activeMenu === "ผู้นวดทั้งหมด" ? "0px" : "110px",
+            }}
+          />
+        )}
+        <MainTable>
+          {activeMenu === "ผู้นวดทั้งหมด" && <Allmassage />}
+          {activeMenu === "Cancelled" && <AllCancelled />}
+        </MainTable>
+      </MassageBody>
+
+      <Modal
+        title={
+            <h1 style={{ textAlign: 'center', marginBottom: '10px' }}>
+              เพิ่มข้อมูลผู้นวด
+            </h1>
+          }
+        visible={modalVisible}
+        onOk={handleModalOk}
+        onCancel={handleModalCancel}
+        destroyOnClose
+        centered
+        maskStyle={{ backgroundColor: 'rgba(0, 0, 0, 0.5)' }}
+        bodyStyle={{ backgroundColor: '#fff', padding: '20px 20px 5px', borderRadius: '5px' }}
+        width={650}
+        footer={[
+            <div key="footer-buttons" style={{ textAlign: 'center' }}>
+             <Button
+  key="submit"
+  type="primary"
+  onClick={handleModalOk} // Change handleOk to handleModalOk
+  style={{
+    borderRadius: '48px',
+    backgroundColor: '#617A55',
+    color: '#fff',
+    height: '50px',
+    width: '125px',
+    transition: 'background-color 0.3s ease',
+  }}
+  onMouseEnter={(e) => {
+    e.target.style.backgroundColor = '#496841';
+  }}
+  onMouseLeave={(e) => {
+    e.target.style.backgroundColor = '#617A55';
+  }}
+>
+  บันทึก
+</Button>
+            </div>
+          ]}
+        >
+        {/* Add your modal content here */}
+        <Form form={form} layout="vertical">
+        <Row gutter={[16, 16]}>
+          <Col span={12}>
+            <Form.Item label="ชื่อ" name="name">
+              <Input
+                style={{
+                  height: '40px',
+                  borderRadius: '48px',
+                  backgroundColor: '#C2D9BD',
+                }}
+                placeholder=""
+              />
+            </Form.Item>
+          </Col>
+          <Col span={12}>
+            <Form.Item label="นามสกุล" name="surname">
+              <Input
+                style={{
+                  height: '40px',
+                  borderRadius: '48px',
+                  backgroundColor: '#C2D9BD',
+                }}
+                placeholder=""
+              />
+            </Form.Item>
+          </Col>
+          <Col span={12}>
+            <Form.Item label="อายุ" name="age">
+              <Input
+                style={{
+                  height: '40px',
+                  borderRadius: '48px',
+                  backgroundColor: '#C2D9BD',
+                }}
+                placeholder=""
+              />
+            </Form.Item>
+          </Col>
+          <Col span={12}>
+            <Form.Item label="ตำแหน่งที่รับผิดชอบ" name="skill">
+              <Input
+                style={{
+                  height: '40px',
+                  borderRadius: '48px',
+                  backgroundColor: '#C2D9BD',
+                }}
+                placeholder=""
+              />
+            </Form.Item>
+          </Col>
+          <Col span={12}>
+          <Form.Item label="เพศ" name="gender">
+          <Radio.Group>
+            <Radio value="male">ชาย</Radio>
+            <Radio value="female">หญิง</Radio>
+            <Radio value="other">เพศทางเลือก</Radio>
+          </Radio.Group>
+        </Form.Item>
+        </Col>
+      
+          {/* Add more form fields here */}
+        </Row>
+        <Col>
+        <Form.Item label="ประสบการณ์ในการทำงาน" name="additionalInfo">
+          <TextArea
+            style={{
+              borderRadius: '8px',
+              backgroundColor: '#C2D9BD',
+              
+            }}
+            placeholder="กรุณากรอกข้อมูล"
+            rows={6}
+          />
+        </Form.Item>
+        </Col>
+      </Form>
+      </Modal>
+    </>
+  );
 };
-  
-  
 
 export default Massage;
